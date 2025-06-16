757549481ec28bea94c23376259c977da763b117
Jefferson-9 <sanchezjefferson999@gmail.com>
2025-06-16 11:54:36 -0500

    Subo la clase PrestamoEducativo

---
+++ ManejoHerencia/src/paquete3/PrestamoEducativo.java
@@ -0,0 +1,28 @@
+package ejecutarprestamo;
+
+public class PrestamoEducativo extends Prestamo {
+    private String nivelEstudio;
+    private InstitucionEducativa centroEducativo;
+    private double valorCarrera;
+    private double valorMensual;
+
+    public PrestamoEducativo(String nivelEstudio, InstitucionEducativa centroEducativo, double valorCarrera, Persona beneficiario, int tiempoMeses, String ciudad) {
+        super(beneficiario, tiempoMeses, ciudad.toUpperCase());
+        this.nivelEstudio = nivelEstudio;
+        this.centroEducativo = centroEducativo;
+        this.valorCarrera = valorCarrera;
+        calcularValorMensual();
+    }
+    
+    public void calcularValorMensual() {
+        double base = valorCarrera / tiempoMeses;
+        valorMensual = base - (base * 0.10);
+    }
+    
+    @Override
+    public String toString() {
+        return
+        String.format("=== Préstamo Educativo ===\n%s\nNivel: %s\nCentro: %s\nValor Carrera: %.2f\nValor mensual: %.2f",
+                super.toString(), nivelEstudio, centroEducativo, valorCarrera, valorMensual);
+    }
+}
