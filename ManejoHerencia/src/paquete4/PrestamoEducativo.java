0342144bb1fbfe48b5a64919606699c881595627
Jefferson-9 <sanchezjefferson999@gmail.com>
2025-06-16 11:43:26 -0500

    Subo la clase PrestamoEducativo

--- ManejoHerencia/src/paquete4/PrestamoEducativo.java
+++ ManejoHerencia/src/paquete4/PrestamoEducativo.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package paquete4;
+package ejecutarprestamo;
 
-/**
- *
- * @author reroes
- */
-public class PrestamoEducativo {
+public class PrestamoEducativo extends Prestamo {
+    private String nivelEstudio;
+    private InstitucionEducativa centroEducativo;
+    private double valorCarrera;
+    private double valorMensual;
+
+    public PrestamoEducativo(String nivelEstudio, InstitucionEducativa centroEducativo, double valorCarrera, Persona beneficiario, int tiempoMeses, String ciudad) {
+        super(beneficiario, tiempoMeses, ciudad.toUpperCase());
+        this.nivelEstudio = nivelEstudio;
+        this.centroEducativo = centroEducativo;
+        this.valorCarrera = valorCarrera;
+        calcularValorMensual();
+    }
     
+    public void calcularValorMensual() {
+        double base = valorCarrera / tiempoMeses;
+        valorMensual = base - (base * 0.10);
+    }
+    
+    @Override
+    public String toString() {
+        return
+        String.format("=== Préstamo Educativo ===\n%s\nNivel: %s\nCentro: %s\nValor Carrera: %.2f\nValor mensual: %.2f",
+                super.toString(), nivelEstudio, centroEducativo, valorCarrera, valorMensual);
+    }
 }
